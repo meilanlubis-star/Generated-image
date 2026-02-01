2c7bfcf555dde622ce4c31a20ca9c99a4736dec7
meilanlubis-star <meilanlubis@gmail.com>
2026-02-01 16:15:03 +0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Ganti 'Generated-image' sesuai dengan nama repositori GitHub Anda
-export default defineConfig({
-  plugins: [react()],
-  base: '/Generated-image/', 
-})
